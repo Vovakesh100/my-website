de1586a0cf9801729e3643bf99a3453892fe16a6
vova <vovakesh100@gmail.com>
2023-08-10 11:48:54 +0300

    change in web-site

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,17 @@
 const hamburger = document.querySelector('.hamburger'),
 	menu = document.querySelector('.menu'),
-	closeElem = document.querySelector('.menu__close');
+	closeElem = document.querySelector('.menu__close'),
+	menuLink = document.querySelector('.menu__list');
 
 hamburger.addEventListener('click', () => {
 	menu.classList.add('active');
 });
 
 closeElem.addEventListener('click', () => {
+	menu.classList.remove('active');
+});
+
+menuLink.addEventListener('click', () => {
 	menu.classList.remove('active');
 });
 
